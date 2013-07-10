6073c2b6d38ca2b66ff4cdaae505caac8bd6c5e5
KetchupMoose <brianallenmarketing@gmail.com>
2013-07-10 14:18:50 +0200

    add file here

---
+++ petGallery/MyPetsQueryTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  MyPetsQueryTableViewController.h
+//  petGallery
+//
+//  Created by mac on 7/9/13.
+//  Copyright (c) 2013 bricorp. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+#import "MyPetDetailsViewController.h"
+@interface MyPetsQueryTableViewController : PFQueryTableViewController <MyPetDetailsViewControllerDelegate>
+
+
+
+
+@end
