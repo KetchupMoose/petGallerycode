1b42cfaf3c0dd60f0706909ebdf5782d1437aaed
KetchupMoose <brianallenmarketing@gmail.com>
2013-07-27 00:46:43 +0200

    uploading work July 27
    uploading lotsa changes

---
+++ petGallery/FrontPageSelectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  FrontPageSelectionViewController.h
+//  petGallery
+//
+//  Created by mac on 7/26/13.
+//  Copyright (c) 2013 bricorp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FrontPageSelectionViewController : UIViewController
+
+@end
---
+++ petGallery/FrontPageSelectionViewController.m
@@ -0,0 +1,38 @@
+//
+//  FrontPageSelectionViewController.m
+//  petGallery
+//
+//  Created by mac on 7/26/13.
+//  Copyright (c) 2013 bricorp. All rights reserved.
+//
+
+#import "FrontPageSelectionViewController.h"
+
+@interface FrontPageSelectionViewController ()
+
+@end
+
+@implementation FrontPageSelectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
